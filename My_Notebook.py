16d73e3fea93478afa30d38fa8ec41c38c220925
ros3riv3r <139646266+ros3riv3r@users.noreply.github.com>
2023-07-29 13:11:21 -0500

    Add files via upload

---
+++ My_Notebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My First Program
+
+# In[1]:
+
+
+1+1
+
+
+# In[2]:
+
+
+5*3
+
+
+# x = min(5, 10, 25)
+# y = max(5, 10, 25)
+# 
+# print(x)
+# print(y)
+
+# In[1]:
+
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+# In[2]:
+
+
+x = pow(4, 3)
+
+print(x)
+
+
+# In[ ]:
+
+
+
+
